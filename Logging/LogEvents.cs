eb013b157079dd7614d558c3c5692db6f5216296
Tatham Oddie <tatham@oddie.com.au>
2013-05-21 09:39:50 +1000

    Build basic event structure

---
+++ Logging/LogEvents.cs
@@ -0,0 +1,19 @@
+﻿namespace Logging
+{
+    public static class LogEvents
+    {
+        public class LogEvent
+        {
+            public long EventId { get; set; }
+            public string ShortDescription { get; set; }
+            public string OperationalGuidance { get; set; }
+        }
+
+        public static readonly LogEvent ExpiredAuthenticationContext = new LogEvent
+        {
+            EventId = 1234,
+            ShortDescription = "The authentication context is beyond its expiry date and can't be used.",
+            OperationalGuidance = "Check the time coordination between the front-end web servers and the authentication tier."
+        };
+    }
+}
---
+++ Tests/LogEventsTests.cs
@@ -0,0 +1,16 @@
+﻿using Logging;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LogEventsTests
+    {
+        [Test]
+        public void ShouldBeAbleToAccessIndividualId()
+        {
+            var id = LogEvents.ExpiredAuthenticationContext.EventId;
+            Assert.AreEqual(1234, id);
+        }
+    }
+}
