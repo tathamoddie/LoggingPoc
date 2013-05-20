d11e31b4af285df1d2e75c88e887fa8d2903e691
Tatham Oddie <tatham@oddie.com.au>
2013-05-21 09:54:00 +1000

    Add convention test examples

---
+++ Tests/ConventionTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Logging;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ConventionTests
+    {
+        [Test]
+        [TestCaseSource(typeof(LogEvents), "AllEvents")]
+        public void AllEventsShouldHaveUniqueIds(LogEvents.LogEvent logEvent)
+        {
+            var numberOfTimesThisEventIdIsUsed = LogEvents
+                .AllEvents
+                .Count(l => l.EventId == logEvent.EventId);
+
+            Assert.AreEqual(
+                1, numberOfTimesThisEventIdIsUsed,
+                "The id was used multiple times. Log event ids must be unique.");
+        }
+
+        [Test]
+        [TestCaseSource(typeof(LogEvents), "AllEvents")]
+        public void AllEventsShouldHaveShortDescription(LogEvents.LogEvent logEvent)
+        {
+            Assert.IsFalse(string.IsNullOrWhiteSpace(logEvent.ShortDescription));
+        }
+
+        [Test]
+        [TestCaseSource(typeof(LogEvents), "AllEvents")]
+        public void AllEventsShouldHaveAtLeast50CharactersOfOperationalGuidance(LogEvents.LogEvent logEvent)
+        {
+            Assert.IsTrue(logEvent.OperationalGuidance.Length >= 50);
+        }
+    }
+}
